e4374ea1d83c59537d27ab78b1f734053f302e72
Nick <nickvandewaterlaat@hotmail.com>
2019-02-20 16:24:20 +0100

    Added attack structure. Removed some tests.

---
+++ src/attacks/arp_attack.py
@@ -0,0 +1,1 @@
+# TODO: Add ARP attack implementation that is executed on a @Host
---
+++ src/attacks/dns_attack.py
@@ -0,0 +1,2 @@
+# TODO: Add DNS attack implementation that is executed on a @Host (?)
+# If we do this, then we have to make sure that it is possible for the MAC address of a host to be empty/None
