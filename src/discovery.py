a4cd4c014cf8f475d868e618ab2aa85f5964e740
Nick <nickvandewaterlaat@hotmail.com>
2019-02-22 13:22:10 +0100

    Changed timeout

--- src/discovery.py
+++ src/discovery.py
@@ -7,5 +7,5 @@ def arp_ping(netmask="192.168.1.0/24"):
     Returns the IP and MAC-address of all local hosts under with respect to @netmask
     """
     conf.verb = 0
-    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=netmask), timeout=2)
+    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=netmask), timeout=0.1)
     return [rcv.sprintf(r"%Ether.src% at %ARP.psrc%") for snd, rcv in ans]
